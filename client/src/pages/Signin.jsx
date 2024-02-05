28946c352bce08a06dee18634d45fbef87a4e9e3
Nitishmodi <nitishmodi200@gmail.com>
2024-02-05 20:00:36 +0530

    complete sign in page functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,88 @@
+import { Alert, Label, Spinner } from 'flowbite-react';
 import React from 'react'
-
+import { Link,useNavigate } from 'react-router-dom';
+import { TextInput } from 'flowbite-react';
+import { Button } from 'flowbite-react';
+import { useState } from 'react';
 export default function Signin() {
+  const [formData,setFormData]=useState({});
+  const [errorMessages,setErrorMessages]=useState(null);
+  const [loading,setLoading]=useState(false);
+  const navigate=useNavigate();
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim()});
+  };
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    if(!formData.email || !formData.password){
+      return  setErrorMessages('Please fill out all fields.');
+    }
+    try{
+      setLoading(true);
+      setErrorMessages(null);
+      const res=await fetch('/api/auth/signin',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify(formData),
+      });
+      const data=await res.json();
+      if(data.success===false){
+        return setErrorMessages(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/');
+      }
+    }catch(error){
+       setErrorMessages(error.message);
+       setLoading(false);
+    }
+  }
   return (
-    <div>Signin</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        <div className='flex-1'>
+           <Link to="/" className=' font-bold dark:text-white text-4xl'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Modi's</span>
+        Blog
+    </Link>
+   <br></br>
+    <br></br>
+    <p className='text-sm mt:5'>Welcome! to the Signin page for My Blog's website. You can sign in with your email and password or with Google Account.</p>
+        </div>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+           
+            <div>
+              <Label value='Your email' />
+              <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange}/>
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput type='password' placeholder='*************' id='password' onChange={handleChange}/>
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span></>
+                ): 'Sign In'
+              }
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Don't Have an account?</span>
+            <Link to='/sign-up' className='text-blue-500 font-bold'>Sign up</Link>
+          </div>
+          {
+           errorMessages && (
+            <Alert className='mt-5' color={'failure'}>{errorMessages}</Alert>
+           )
+          }
+        </div>
+      </div>
+     </div>
+    
   )
 }
