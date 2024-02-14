52536ed1102edafa55560bdc9109db0e06b41a9d
Nitishmodi <nitishmodi200@gmail.com>
2024-02-14 16:56:49 +0530

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,45 +1,41 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
-export const test=(req,res)=>{
-    res.json({message:'API is working!'});
+export const test = (req, res) => {
+    res.json({ message: 'API is working!' });
 };
-export const updateUser=async(req,res,next)=>{
-    if(req.user.id!=req.params.userId){
-        return next(errorHandler(403,'You are not allowed to update this user'));
+export const updateUser = async (req, res, next) => {
+    try {
+        if (req.user.id !== req.params.userId) {
+            return next(errorHandler(403, 'You are not allowed to update this user'));
+        }
+        const { username, email, profilePicture, password } = req.body;
+        if (password && password.length < 6) {
+            return next(errorHandler(400, 'Password must be at least 6 characters long'));
+        }
+        if (username) {
+            if (username.length < 7 || username.length > 20) {
+                return next(errorHandler(400, "Username should have length between 7 and 20"));
+            }
+            if (/\s/.test(username)) {
+                return next(errorHandler(400, "Username can't contain spaces"));
+            }
+            if (username !== username.toLowerCase()) {
+                return next(errorHandler(400, "Username should be in lower case"));
+            }
+            if (!/^[a-zA-Z0-9]+$/.test(username)) {
+                return next(errorHandler(400, "Username can only contain letters and numbers"));
+            }
+        }
+        const updateFields = {};
+        if (username) updateFields.username = username;
+        if (email) updateFields.email = email;
+        if (profilePicture) updateFields.profilePicture = profilePicture;
+        if (password) updateFields.password = bcryptjs.hashSync(password, 10);
+        const updatedUser = await User.findByIdAndUpdate(req.params.userId, { $set: updateFields }, { new: true });
+        const { password: userPassword, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
     }
-    if(req.body.password){
-        if(req.body.password.length<6){
-            return next(errorHandler(400,'Password must be of atleast 6 characters'))
-        }
-        req.body.password = bcryptjs.hashSync(req.body.password,10);
-    }
-    if(req.body.username){
-        if(req.body.username.length<7 || req.body.username.length>20){
-            return next(errorHandler(400,"Username should have length between 7 and 20"));
-        }
-        if(req.body.username.includes(' ')){
-            return next(errorHandler(400,"Username can't contain spaces"));
-        }
-        if(req.body.username!==req.body.username.toLowerCase()){
-            return next(errorHandler(400,"Username should be in lower case"));
-        }
-        if(req.body.username.match(/^[a-zA-Z0-9]+$/)){
-            return  next(errorHandler("Username can only contain letters and numbers"));
-        }
-        try{
-            const updateUser=await User.findByIdAndUpdate(req.params.userId,{
-                $set:{
-                    username:req.body.username,
-                    email:req.body.email,
-                    profilePicture:req.body.profilePicture,
-                    password:req.body.password,
-                },
-            },{new:true});
-            const {password,...rest}=updateUser._doc;
-            res.status(200).json(rest);
-        }catch(error){
-            next(error);
-        }
-    }
-}
+};
